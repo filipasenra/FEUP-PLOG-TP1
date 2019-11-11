431f594f5a97a2c50ebc4b0ef6db82b455c0d745
filipasenra <filipasenra4@gmail.com>
2019-11-11 12:01:53 +0000

    Minor alteration in Base Case of PrintMatrix

--- src/board.pl
+++ src/board.pl
@@ -4,7 +4,7 @@ printBoard([Head | Tail]) :-
     length(Head, LenList),
     writeNumbersRow(LenList, 0),
     writeDivisions(LenList),
-    printMatrix([Head | Tail], 0).
+    printMatrix([Head | Tail], 0, LenList).
 
 
 %Writing Divisions%
@@ -54,25 +54,24 @@ writeEmptySpaces(N) :-
 
 
 %Prints the Matrix
-printMatrix([], N) :-
+printMatrix([], N, LenList) :-
     writeNumber(N),
+    writeEmptySpaces(LenList),
     write('\n').
 
-printMatrix([Head | Tail], 0) :-
+printMatrix([Head | Tail], 0, LenList) :-
     writeNumber(0),
-    length(Head, LenList),
     writeEmptySpaces(LenList),
     writeDivisions(LenList),
-    printMatrix([Head | Tail], 1).
+    printMatrix([Head | Tail], 1, LenList).
 
-printMatrix([Head | Tail], N) :-
+printMatrix([Head | Tail], N, LenList) :-
     writeNumber(N),
     write('     |'),
     printLine(Head),
-    length(Head, LenList),
     writeDivisions(LenList),
     N1 is N+1,
-    printMatrix(Tail, N1).
+    printMatrix(Tail, N1, LenList).
 
 
 %Prints each Line
